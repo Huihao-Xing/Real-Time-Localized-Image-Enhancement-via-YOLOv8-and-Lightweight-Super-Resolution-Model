795fedbe83b46a65ddf7afb0b0b58e2ce7ceda2d
Huihao Xing <119607601+Huihao-Xing@users.noreply.github.com>
2025-05-02 21:44:44 -0400

    Add files via upload

--- train_gpu.py
+++ train_gpu.py
@@ -3,22 +3,22 @@ import torch
 
 
 def main():
-    # 检查CUDA
-    print(f"PyTorch CUDA可用: {torch.cuda.is_available()}")
+    # check pug
+    print(f"PyTorch CUDA avail: {torch.cuda.is_available()}")
 
-    # 加载模型
+    # load model
     model = YOLO('yolov8n-seg.pt').to('cuda')
-    print(f"模型设备: {next(model.model.parameters()).device}")  # 应输出cuda:0
+    print(f"model device: {next(model.model.parameters()).device}")
 
-    # 训练配置（关键修改：workers=0或1）
+    # parameters setting
     model.train(
         data='config.yaml',
         epochs= 10,
         imgsz=640,
-        workers=1,  # Windows必须设为0或1
-        device='cuda'  # 显式指定
+        workers=1,
+        device='cuda'
     )
 
 
 if __name__ == '__main__':
-    main()  # Windows多进程必须的保护
+    main()  
